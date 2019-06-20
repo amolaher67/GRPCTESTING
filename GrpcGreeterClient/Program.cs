cb570d49cf01e862d63b928fee73159c7c804771
Amol Aher <amola@winjit.com>
2019-06-20 14:43:57 +0530

    Added clinet project as well

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",true);
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:50051");
+            var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
+
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+                                 
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            using (var call = client.lotsOfReplies(new HelloRequest { Name = "GreeterClient" }))
+            {
+                while(await call.ResponseStream.MoveNext())
+                {
+                    var replystreammsg = call.ResponseStream.Current;
+                    Console.WriteLine("Received " + replystreammsg.ToString());
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
